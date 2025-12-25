7ec9b4a6119afda38f5fe1dbc5370ed2747de5b6
HristoSLV <hristoivanovslavchev@gmail.com>
2025-12-25 11:54:49 +0200

    25/12/2025 New plugin test pt3

--- Topics/Input streams/Count words/src/Main.java
+++ Topics/Input streams/Count words/src/Main.java
@@ -5,6 +5,15 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         // start coding here
+        String line = reader.readLine().trim();
+
+        if (line.isEmpty()) {
+            System.out.println(0);
+        } else {
+            String[] words = line.split("\\s+");
+            System.out.println(words.length);
+        }
+
         reader.close();
     }
 }
