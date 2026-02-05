55a8103e5719feb45f0d56ecd0c6855fe9a199ae
HristoSLV <99892358+HristoSLV@users.noreply.github.com>
2026-02-05 12:15:28 +0200

    Custom annotations (#3)
    04.02.2026 Completed Custom annotations and types of annotations

---
+++ Topics/Custom annotations and types of annotations/Printing arguments annotation/src/Main.java
@@ -0,0 +1,21 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.METHOD})
+@interface PrintArgs {
+    boolean printReturn();
+}
+
+
+class TestClass {
+    @PrintArgs(printReturn = true)
+    public void myMethod() {
+        // some code
+    }
+}
+
+
+
