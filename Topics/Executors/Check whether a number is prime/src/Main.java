1cda0bc431e3a44fa1cb40dd3882b428b90aa75c
HristoSLV <hristoivanovslavchev@gmail.com>
2026-02-11 19:59:04 +0200

    11.02.2026 Exceptions in threads, Executors, Thread synchronization

---
+++ Topics/Executors/Check whether a number is prime/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        while (scanner.hasNext()) {
+            int number = scanner.nextInt();
+            // create and submit tasks
+            executor.submit(new PrintIfPrimeTask(number));
+        }
+        executor.shutdown();
+    }
+}
+
+class PrintIfPrimeTask implements Runnable {
+    private final int number;
+
+    public PrintIfPrimeTask(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        // write code of task here
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime && number > 1) {
+            System.out.println(number);
+        }
+
+    }
+}
