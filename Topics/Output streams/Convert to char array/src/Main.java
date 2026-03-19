921694c440d3fe815629fddfabb5c2d02a8aaaf8
HristoSLV <hristoivanovslavchev@gmail.com>
2026-03-19 21:10:30 +0200

    19.03.2026 Output streams

---
+++ Topics/Output streams/Convert to char array/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.CharArrayWriter;import java.io.IOException;
+
+class Converter {
+    public static char[] convert(String[] words) throws IOException {
+        // implement the method
+        CharArrayWriter writer = new CharArrayWriter();
+
+        for (String word : words) {
+            writer.write(word);
+        }
+
+        return writer.toCharArray();
+    }
+}
